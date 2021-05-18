cbcaec0ca1c4e48b94a15205c2912fb86c2d8438
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-05-18 01:37:52 -0300

    modifications @ app.component.html, app.component.ts, city.ts, environment.ts

--- register-app/src/app/app.component.ts
+++ register-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { City } from './city'
+import { CityService } from './city.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'register-app';
+export class AppComponent implements OnInit{
+  public cities: City[] = [];
+
+  constructor(private cityService: CityService){}
+
+  ngOnInit() {
+    this.getCity();
+  }
+
+  public getCity(): void{
+    this.cityService.getCities().subscribe(
+      (response: City[])=>{
+        this.cities = response;
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    )
+  }
+
 }
+
--- register-app/src/app/city.ts
+++ register-app/src/app/city.ts
@@ -1,4 +1,4 @@
-export interface City{
+export interface city{
     id: number;
     codIbge: string;
     nomeCidade: string;
